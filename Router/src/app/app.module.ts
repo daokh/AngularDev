959ef383ead2b57a96a1bb0503c4341c601accbd
ALIENWARE\khanh <khanh.dao@gmail.com>
2024-06-29 17:53:08 -0700

    Added Router project

---
+++ Router/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DefaultComponent } from './components/default/default.component';
+
+import { appRoutes } from './routerConfig';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    DashboardComponent,
+    DefaultComponent,
+  ],
+  imports: [
+    BrowserModule, RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
